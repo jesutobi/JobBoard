ee2b16478624f54485a9922dcfd922fe76024a69
MAC <jesutobit@gmail.com>
2023-12-12 01:05:44 +0100

    update

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class ListingController extends Controller
+{
+    //show all listings
+    public function index(){
+        // dd(Listing::latest()->filter(request(['tag', 'search']))->paginate(5));
+        return view('listings.index',[
+        'listings' => Listing::latest()->filter(request(['tag', 'search']))->paginate(6)
+        
+    ]);
+    
+    }
+    //show single listing
+    public function show(Listing $listing){
+     return view('listings.show',[
+        'listing' => $listing
+        
+    ]);
+    }
+
+    //show create form
+    public function create(){
+     return view('listings.create');
+    }
+
+    //store listing data
+    public function store(Request $request){
+      
+     $formfields = $request->validate([
+        'title' =>'required',
+        'company' =>['required', Rule::unique('listings','company')],
+        'location' =>'required',
+        'website' =>'required',
+        'email' => ['required', 'email'],
+        'tags' =>'required',
+        'description' =>'required'
+     ]);
+
+    if($request->hasFile('logo')) {
+        $formfields['logo'] = $request->file('logo')->store('logos', 'public');
+    }
+
+     Listing::create($formfields);
+        return redirect('/')->with('message', 'Listing created successfuly!');
+     
+    }
+
+    // show edit form
+    public function edit(Listing $listing){
+        // dd($listing->title);
+        return view('listings.edit', ['listing' => $listing]); 
+    }
+
+    //update listing data
+    public function update(Request $request, Listing $listing) {
+      
+     $formfields = $request->validate([
+       'title' => 'required',
+        'company' => ['required'],
+        'location' => 'required',
+        'website' => 'required',
+        'email' => ['required', 'email'],
+        'tags' => 'required',
+        'description' => 'required'
+     ]);
+
+        if($request->hasFile('logo')) {
+            $formfields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+     $listing->update($formfields);
+
+    return back()->with('message', 'Listing edited successfuly!');
+     
+    }
+
+    public function delete(Listing $listing){
+        $listing->delete();
+        return redirect('/')->with('message','Listing deleted successfully');
+
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Models\Listing;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
+use App\Http\Controllers\ListingController;
+use App\Http\Controllers\ProfileController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,18 +18,46 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// all listings
+Route::get('/',[ListingController::class, 'index']);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// create form
+Route::get('/listings/create', [ListingController::class, 'create']);
 
-require __DIR__.'/auth.php';
+// create job
+Route::post('/listings', [ListingController::class, 'store']);
+
+
+// single listing
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+// show edit form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+// delete listing
+Route::delete('/listings/{listing}', [ListingController::class, 'delete']);
+
+// update listing
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
+// show register form
+Route::get('/register', [UserController::class, 'register']);
+
+// new users
+Route::post('/users', [UserController::class, 'store']);
+
+
+
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+// require __DIR__.'/auth.php';
